5c168d6d27a5b6db418adc2dcbad0170bbf88608
projeto_django <deividbertapele@gmail.com>
2022-10-10 09:56:55 -0300

    atualizacao_dotenv

--- upload_arquivo/settings.py
+++ upload_arquivo/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0(f_8@0jw(guk-3woq9h!al_^&-k#9uvnt)ga@-nee1u_8*o$-"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
